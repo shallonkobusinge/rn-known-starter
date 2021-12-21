c294bd2e187bbd14f277795801d6a1128be1d809
shallon kobusinge <kobshallon041@gmail.com>
2021-12-21 19:18:45 +0200

    [FEAT] navigations

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,16 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen</Text>;
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View>
+
+      <Button title="Go to Component screen" onPress={() => navigation.navigate("Components")} />
+      <TouchableOpacity onPress={() => navigation.navigate("List")}>
+        <Text>Go to List screen</Text>
+      </TouchableOpacity>
+    </View >
+  )
 };
 
 const styles = StyleSheet.create({
