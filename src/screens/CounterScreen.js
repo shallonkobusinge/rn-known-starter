e4fadca71e2d20e2e4d91f8804f955be775fa41a
shallon kobusinge <kobshallon041@gmail.com>
2022-01-02 22:51:19 +0200

    [FEAT] userReducer in counter

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "increment":
+            return { ...state, counter: state.counter + action.payload };
+
+        case "decrement":
+            return { ...state, counter: state.counter - action.payload };
+        default:
+            return state;
+    }
+}
+
 const CounterScreen = () => {
-    const [counter, setCounter] = React.useState(0);
+
+    const [state, dispatch] = useReducer(reducer, { counter: 0 });
+
     return <View>
-        <Button title="Increase" onPress={() => { setCounter(counter + 1) }} />
-        <Button title="Decrease" onPress={() => { setCounter(counter - 1) }} />
+        <Button title="increment" onPress={() => { dispatch({ type: "increment", payload: 1 }) }} />
+        <Button title="decrement" onPress={() => { dispatch({ type: "decrement", payload: 1 }) }} />
+        <Text>Current counter : {state.counter}</Text>
     </View>
 }
 
